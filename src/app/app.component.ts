215cb5074e9644e095c04177d8e7295c80d4d14f
Yanick4 <yanickleoneltamegni@gmail.com>
2025-01-28 08:07:52 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OneSignalService } from './services/one-signal.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test_notification_push';
+  oneSignal=inject(OneSignalService)
+
+  ngOnInit(): void {
+      this.oneSignal.initOneSignal()
+  }
+
 }
---
+++ src/app/services/one-signal.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { OneSignal } from "onesignal-ngx"
+@Injectable({
+  providedIn: 'root'
+})
+export class OneSignalService {
+  private oneSignal=inject(OneSignal)
+
+  public initOneSignal(){
+    this.oneSignal.init({
+      appId: "6813c394-dfeb-4141-976a-74c9f6178f8a",
+      serviceWorkerParam: { scope: '../../' },
+      serviceWorkerPath:'OneSignalSDKWorker.js'
+    })
+  }
+}
