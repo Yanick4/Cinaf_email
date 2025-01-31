ddeb0fdb68bd02a37a1e2097a9b5c48fd6090bc3
Yanick4 <yanickleoneltamegni@gmail.com>
2025-01-31 11:56:41 +0100

    New release

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    api_url:"https://51c5-2a0d-3344-12af-2b10-acb8-521b-b9e3-5913.ngrok-free.app "
+    api_url:"https://51c5-2a0d-3344-12af-2b10-acb8-521b-b9e3-5913.ngrok-free.app"
 };
