874071f3ce86f8e58b0ae9938daed255a6312db4
Yanick4 <yanickleoneltamegni@gmail.com>
2025-01-31 10:39:27 +0100

    Update api link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    api_url:"https://44d4-2a0d-3344-12af-2b10-65c4-aef4-b3b9-46e9.ngrok-free.app"
+    api_url:"https://51c5-2a0d-3344-12af-2b10-acb8-521b-b9e3-5913.ngrok-free.app "
 };
