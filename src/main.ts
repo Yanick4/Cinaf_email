d36f5a2b4f56aabe8503aefcdb6fdbd1d299a173
Yanick4 <yanickleoneltamegni@gmail.com>
2025-03-25 16:30:07 +0100

    release for angular

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker enregistré avec succès :', registration);
+    })
+    .catch((error) => {
+      console.error('Erreur lors de l’enregistrement du Service Worker :', error);
+    });
+}
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
