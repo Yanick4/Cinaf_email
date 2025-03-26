76a6b44a4102a6ef93f0f81570a04adbd543b834
Yanick4 <yanickleoneltamegni@gmail.com>
2025-03-26 16:14:15 +0100

    another relase

--- src/app/services/one-signal.service.ts
+++ src/app/services/one-signal.service.ts
@@ -73,13 +73,14 @@ export class OneSignalService{
       if ('Notification' in window) {
         if (Notification.permission === "granted") {
           new Notification(title, options);
-        } else if (Notification.permission !== "denied") {
-          Notification.requestPermission().then((permission) => {
-            if (permission === "granted") {
-              new Notification(title, options);
-            }
-          });
-        }
+        } 
+        // else if (Notification.permission !== "denied") {
+        //   Notification.requestPermission().then((permission) => {
+        //     if (permission === "granted") {
+        //       new Notification(title, options);
+        //     }
+        //   });
+        // }
       } else {
         console.log("L'API des Notifications n'est pas prise en charge par ce navigateur.");
       }
